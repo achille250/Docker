51d4027eebe2de80eac0aec47b17ad04ba3122f4
achille250 <106239625+achille250@users.noreply.github.com>
2023-07-23 20:42:24 +0200

    Docker- compose

---
+++ My AI/Back-end/server.js
@@ -0,0 +1,13 @@
+//set up libraries/packages
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain' });
+    res.end('Hello from Backend!');
+});
+
+const PORT= 8080;
+
+server.listen(PORT,() =>{
+    console.log('Server running on port ${PORT}');
+});
---
+++ My AI/compod/be/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const PORT = 8080;
+
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+  res.send('Hello from Backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
